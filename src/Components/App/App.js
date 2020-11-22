d270b2fda7f93705df65b2a286446d2e83901379
Karen Garcia <saykaren@gmail.com>
2020-11-22 08:15:29 -0700

    Updating

---
+++ src/Components/App/App.js
@@ -0,0 +1,16 @@
+import '../Styling/App.scss';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+   Cheatsheet
+      </header>
+      <section>
+        Hello
+      </section>
+    </div>
+  );
+}
+
+export default App;
