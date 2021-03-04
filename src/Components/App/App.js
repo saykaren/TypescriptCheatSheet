df146f43ff424262a314779e3d3468ffc7f28504
Karen Garcia <saykaren@gmail.com>
2021-03-03 21:12:43 -0700

    SASS Setup Details Added SASS setup details easy to follow

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -112,6 +112,19 @@ function App() {
         </div>
         <div className="instructionalBox">
           <h2>Add SASS Steps</h2>
+          <ul>
+            <li>Add with git command: yarn add node-sass</li>
+            <li>Now rename ./src/App.css to ./src/App.scss</li>
+            <li>Open ./src/App.js and you will find import './App.css'. Change it to import './App.<b>scss</b>'</li>
+            <li>Create a sass file name it variables.scss under ./src directory. Typically add a directory called Styling to keep your styling items in.</li>
+            <li>Open variables.scss file and start with $primary-color: #282c34; $secondary-color: #61dafb;</li>
+            <li>Navigate to App.scss and add to the top @import './variables.scss'</li>
+            <li>Now you can add the $primary-color instead a direct color name in your code so if you change colors later it will change all spots.</li>
+            <li>Example .App-link{`{color: $secondary-color;}`}</li>
+          </ul>
+          <p></p>
+          <p></p>
+
         </div>
         <div className="instructionalBox">
           <h2>Filter Fun</h2>
