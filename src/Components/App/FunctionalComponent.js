f4d0126ce873e8590f61b1386b2e219316621675
Karen Garcia <saykaren@gmail.com>
2021-03-06 21:21:19 -0700

    Individual Files for Components Moving components into their own folder for easy viewing

---
+++ src/Components/App/FunctionalComponent.js
@@ -0,0 +1,14 @@
+import React from "react";
+import ComponentSetup from '../Assests/ComponentSetup.jpeg';
+
+const FunctionalComponent = () => {
+
+    return(
+        <div className="instructionalBox">
+        <h2>Functional Component Initial Setup</h2>
+        <img src={ComponentSetup} alt="Component Setup"/>
+        </div>
+    );
+};
+
+export default FunctionalComponent;
