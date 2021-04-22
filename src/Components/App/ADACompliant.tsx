836b7c600cc51ad36555697fc65d8b1e78023c05
Karen Garcia <saykaren@gmail.com>
2021-04-22 16:20:27 -0600

    ADA Compliance Resources

---
+++ src/Components/App/ADACompliant.tsx
@@ -0,0 +1,34 @@
+import React from "react";
+
+const ADACompliant = () => {
+  return (
+    <>
+      <a
+        href="https://www.w3.org/WAI/standards-guidelines/wcag/"
+        rel="noreferrer noopener"
+        target="_blank"
+      >
+        Web Content Accessibility Guidelines
+      </a>
+      <br />
+      <a
+        href="https://www.w3.org/WAI/ER/tools/"
+        rel="noreferrer noopener"
+        target="_blank"
+      >
+        Evaluation Tool List
+      </a>
+      <br />
+      <a
+        href="https://color.a11y.com/Contrast/"
+        rel="noreferrer noopener"
+        target="_blank"
+      >
+        Testing Font Contrast
+      </a>
+      <br />
+    </>
+  );
+};
+
+export default ADACompliant;
