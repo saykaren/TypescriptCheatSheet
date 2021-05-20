a8107610eeff9ddb39299cfe286aabc1440dedf5
Karen Garcia <saykaren@gmail.com>
2021-05-19 20:52:56 -0600

    Add RegEx Resource links

---
+++ src/Components/App/RegEx.tsx
@@ -0,0 +1,30 @@
+import React from "react";
+
+const RegEx = () => {
+  return (
+    <>
+      <a href="https://regexr.com/" rel="noreferrer noopener" target="_blank">
+        RegEx testing
+      </a>
+      <br />
+      <a
+        href="https://www.regextester.com/93821"
+        rel="noreferrer noopener"
+        target="_blank"
+      >
+        RegEx help with examples
+      </a>
+      <br />
+      <a
+        href="https://htmlcheatsheet.com/js/"
+        rel="noreferrer noopener"
+        target="_blank"
+      >
+        JS / CSS / HTML Cheatsheet
+      </a>
+      <br />
+    </>
+  );
+};
+
+export default RegEx;
