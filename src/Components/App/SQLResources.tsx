f2964f80e436ccd7a7277d3f831243f2dfbc03ec
Karen Garcia <saykaren@gmail.com>
2021-06-09 14:54:28 -0600

    Adds SQL resource links

---
+++ src/Components/App/SQLResources.tsx
@@ -0,0 +1,34 @@
+import React from "react";
+
+const SQLResources = () => {
+  return (
+    <>
+      <a
+        href="https://sqlzoo.net/wiki/SELECT_Quiz"
+        rel="noreferrer noopener"
+        target="_blank"
+      >
+        Select Quiz
+      </a>
+      <br />
+      <a
+        href="https://www.youtube.com/watch?v=7S_tz1z_5bA&t=299s"
+        rel="noreferrer noopener"
+        target="_blank"
+      >
+        MySQL Tutorial for Beginners
+      </a>
+      <br />
+      <a
+        href="https://www.sqlteaching.com/#!group_by"
+        rel="noreferrer noopener"
+        target="_blank"
+      >
+        SQL Teaching
+      </a>
+      <br />
+    </>
+  );
+};
+
+export default SQLResources;
