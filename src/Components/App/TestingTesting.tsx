832e5089b2dea60fb0a4102eb7876e4c97a9a70f
Karen Garcia <saykaren@gmail.com>
2021-06-22 15:55:56 -0600

    Adds Testing Resources

--- src/Components/App/TestingTesting.tsx
+++ src/Components/App/TestingTesting.tsx
@@ -22,7 +22,60 @@ const TestingTesting = () => {
 import Adapter from 'enzyme-adapter-react-16';
 
 Enzyme.configure({ adapter: new Adapter() });`}</p>
+<p>Make a Testing folder to put under Components folder</p>
+<p>{`Then add files with .test.js at the end example App.test.js (there should be one like that already move into the testing folder). Also update the import of App to match where App now is in the app. Example import App from './../app/App'; then update their typical test as we don't have /learn react in the app anymore.`}</p>
+      <h3>Example Contents</h3>
+      <p>{`example contents
+import App from "./../App/App";
+import React from "react";
+import { shallow } from "enzyme";
+
+describe("rendering App components", () => {
+it("It renders App component without crashing", () => {
+shallow();
+});
+});`}</p>
+<h2>Example of testing components rendering</h2>
+<p>{`import LandingPage from "./../app/LandingPage";
+import { shallow } from "enzyme";
+
+const wrapper = shallow(<LandingPage activeItem="Home" />);
+
+describe("Render LandingPage", () => {
+  it("It finds the class heroSection on LandingPage component", () => {
+    expect(wrapper.find("h1").prop("id"));
+  });
+
+  it("It switches components depending upon activeItem", () => {
+    var activeItemString = "MyWork";
+    const wrapperMyWork = shallow(
+      <LandingPage activeItem={activeItemString} />
+    );
+    const wrapperAboutMe = shallow(<LandingPage activeItem="AboutMe" />);
+    // console.log(wrapperAboutMe.debug());
+    expect(wrapperMyWork.find("MyWork")).toHaveLength(1);
+    // Changed activeItem to "AboutMe"
+    expect(wrapperAboutMe.find("MyWork")).toHaveLength(0);
+    expect(wrapperAboutMe.find("Home")).toHaveLength(0);
+    expect(wrapperAboutMe.find("AboutMe")).toHaveLength(1);
+  });
+});`}</p>
+<h3>Triage Help: </h3>
+<p>put <br/>{`console.log(wrapperAboutMe.debug());`} <br/>before expect section to see what testing is seeing when you run npn test</p>
+<h3>Running Tests</h3>
+<p>Then to test do npm test (which in package.json you will see it does react-scripts test
+remember CTL+C stops command of testing and others</p>
       <h3>Resources</h3>
+      <a
+        href="https://scotch.io/tutorials/testing-react-components-with-enzyme-and-jest"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        {" "}
+        AWESOME RESOURCE
+      </a>
+      <br />
+      <br />
       <a
         href="https://www.youtube.com/watch?v=XNzVMP7Mnxg"
         target="_blank"
@@ -71,6 +124,27 @@ Enzyme.configure({ adapter: new Adapter() });`}</p>
         {" "}
         How to Setup Jest and Enyme in React App in 5 minutes
       </a>
+      <h3>My Code Examples</h3>
+      <a
+        href="https://github.com/saykaren/Pam/tree/main/src/components/Testing"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        {" "}
+        Pam's Website 
+      </a>
+      <br />
+      <br />
+      <a
+        href="https://github.com/saykaren/BreakingBad/tree/main/src/Components/Testing"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        {" "}
+        Breaking Bad Website 
+      </a>
+      <br />
+      <br />
     </>
   );
 };
