210c6660e770ab4d319ebaf407207ba2f428712f
Karen Garcia <saykaren@gmail.com>
2021-06-23 15:38:38 -0600

    Adds Adjustment

--- src/Components/App/Alias.tsx
+++ src/Components/App/Alias.tsx
@@ -11,7 +11,7 @@ const Alias = () => {
       </p>
       <h2>Add to Config File</h2>
       <p>
-        git config --global alias.pushIt 'git push -u origin HEAD
+        git config --global alias.pushIt 'push -u origin HEAD
         --force-with-lease'
       </p>
       <p>
@@ -30,6 +30,11 @@ const Alias = () => {
         $ git config --global alias.sw switch
         <br />$ git config --global alias.st status
       </p>
+      <h3>Check Alias</h3>
+      <p>git config --get alias.pushit</p>
+      <h4>Adjust Alias</h4>
+      <p>Just do the command that you would to add it</p>
+      <p>git config --global alias.pushit 'push -u origin HEAD --force-with-lease' (I originally added the git in the quotes which messed it up'</p>
       <a
         href="http://git-scm.com/book/en/v2/Git-Basics-Git-Aliases"
         target="_blank"
