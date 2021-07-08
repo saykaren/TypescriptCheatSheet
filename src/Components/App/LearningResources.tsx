3753a9135082c4d971d42facb3acb01228522a70
Karen Garcia <saykaren@gmail.com>
2021-07-08 15:01:55 -0600

    Adds learning exercises

---
+++ src/Components/App/LearningResources.tsx
@@ -0,0 +1,37 @@
+import React from "react";
+
+const LearningResources = () => {
+  return (
+    <>
+      <div className="instructionalBox">
+        <a
+          href="https://coderbyte.com/"
+          rel="noreferrer noopener"
+          target="_blank"
+        >
+          CoderByte
+        </a>
+      </div>
+      <div className="instructionalBox">
+        <a
+          href="https://exercism.io/"
+          rel="noreferrer noopener"
+          target="_blank"
+        >
+          Exercism
+        </a>
+      </div>
+      <div className="instructionalBox">
+        <a
+          href="https://www.codewars.com/"
+          rel="noreferrer noopener"
+          target="_blank"
+        >
+          CodeWars
+        </a>
+      </div>
+    </>
+  );
+};
+
+export default LearningResources;
