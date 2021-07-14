a4ecb0d15c28871448980ed1af57af1935ce0752
Karen Garcia <saykaren@gmail.com>
2021-07-13 20:27:02 -0600

    Adds resources for checking what browsers accept

--- src/Components/App/ADACompliant.tsx
+++ src/Components/App/ADACompliant.tsx
@@ -3,6 +3,17 @@ import React from "react";
 const ADACompliant = () => {
   return (
     <>
+    <ul>
+      <li>
+      <a
+        href="https://caniuse.com/"
+        rel="noreferrer noopener"
+        target="_blank"
+      >
+        Can I Use _______?
+      </a>
+      </li>
+      <li>
       <a
         href="https://www.w3.org/WAI/standards-guidelines/wcag/"
         rel="noreferrer noopener"
@@ -10,7 +21,8 @@ const ADACompliant = () => {
       >
         Web Content Accessibility Guidelines
       </a>
-      <br />
+      </li>
+      <li>
       <a
         href="https://www.w3.org/WAI/ER/tools/"
         rel="noreferrer noopener"
@@ -18,7 +30,8 @@ const ADACompliant = () => {
       >
         Evaluation Tool List
       </a>
-      <br />
+      </li>
+      <li>
       <a
         href="https://color.a11y.com/Contrast/"
         rel="noreferrer noopener"
@@ -26,7 +39,10 @@ const ADACompliant = () => {
       >
         Testing Font Contrast
       </a>
-      <br />
+      </li>
+    </ul>
+
+
     </>
   );
 };
