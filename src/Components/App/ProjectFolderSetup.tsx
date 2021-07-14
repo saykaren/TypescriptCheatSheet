9e4447a127ff809ba0fd342f1d7d8f83ee701db6
Karen Garcia <saykaren@gmail.com>
2021-07-13 19:50:21 -0600

    Adds Folder Structure Resource and better tree

--- src/Components/App/ProjectFolderSetup.tsx
+++ src/Components/App/ProjectFolderSetup.tsx
@@ -4,24 +4,49 @@ const ProjectFolderSetup = () => {
   return (
     <>
       <div className="leftCentered">
-        - src/
-        <br />
-        -- Components/
-        <br />
-        --- App/ (components)
-        <br />
-        ---- App.js
-        <br />
-        ---- Footer.tsx
-        <br />
-        --- Assets/ (images)
-        <br />
-        --- Data/ (data sets)
-        <br />
-        --- Interface/ (inteface objects)
-        <br />
-        --- Styling/ (SCSS files) <br />
-        ---- App.scss <br />
+        <ul>
+          <li>src/</li>
+          <ul>
+            <li>
+              Components/
+              <ul>
+                <li>
+                  App/ (components)
+                  <ul>
+                    <li>App.js</li>
+                    <li>Footer.tsx</li>
+                  </ul>
+                </li>
+                <li>Assets/ (images)</li>
+                <li>Data/ (data sets)</li>
+                <li>Interface/ (inteface objects)</li>
+                <li>
+                  Styling/ (SCSS files)
+                  <ul>
+                    <li>App.scss</li>
+                    <li>variables.scss</li>
+                  </ul>
+                </li>
+                <li>
+                  Testing
+                  <ul>
+                    <li>App.test.js</li>
+                  </ul>
+                </li>
+              </ul>
+            </li>
+          </ul>
+        </ul>
+        <div>
+          <a
+            href="https://medium.com/@Charles_Stover/optimal-file-structure-for-react-applications-f3e35ad0a145"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            {" "}
+            React File Structure
+          </a>
+        </div>
       </div>
     </>
   );
