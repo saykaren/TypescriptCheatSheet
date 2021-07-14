ee5a39a972680090defb06d4141ef1899713e0a2
Karen Garcia <saykaren@gmail.com>
2021-07-13 20:23:34 -0600

    Adds resources for html symbols

--- src/Components/App/CharacterReferences.tsx
+++ src/Components/App/CharacterReferences.tsx
@@ -3,33 +3,59 @@ import React from "react";
 const CharacterReferences = () => {
   return (
     <>
-      <div className="instructionalBox">
-        <a
-          href="https://dev.w3.org/html5/html-author/charref"
-          rel="noreferrer noopener"
-          target="_blank"
-        >
-          Character References
-        </a>
-      </div>
-      <div className="instructionalBox">
-        <a
-          href="https://material.io/resources/icons/?style=baseline"
-          rel="noreferrer noopener"
-          target="_blank"
-        >
-          Icons
-        </a>
-      </div>
-      <div className="instructionalBox">
-        <a
-          href="https://unsplash.com/"
-          rel="noreferrer noopener"
-          target="_blank"
-        >
-          Photos
-        </a>
-      </div>
+      <ul>
+        <li>
+          <a
+            href="https://dev.w3.org/html5/html-author/charref"
+            rel="noreferrer noopener"
+            target="_blank"
+          >
+            Character References
+          </a>
+        </li>
+        <li>
+          <a
+            href="https://material.io/resources/icons/?style=baseline"
+            rel="noreferrer noopener"
+            target="_blank"
+          >
+            Icons
+          </a>
+        </li>
+        <li>
+          <a
+            href="https://unsplash.com/"
+            rel="noreferrer noopener"
+            target="_blank"
+          >
+            Photos
+          </a>
+        </li>
+        <li>
+          Common HTML Symbols
+          <ul>
+            <li>
+              <a
+                href="https://www.w3schools.com/icons/fontawesome5_icons_datetime.asp"
+                rel="noreferrer noopener"
+                target="_blank"
+              >
+                Font Awesome Date and Time
+              </a>
+            </li>
+            <li>
+              <a
+                href="https://www.toptal.com/designers/htmlarrows/symbols/"
+                rel="noreferrer noopener"
+                target="_blank"
+              >
+                HTML Currency Symbols and ASCII Currency Character Code
+                Reference
+              </a>
+            </li>
+          </ul>
+        </li>
+      </ul>
     </>
   );
 };
