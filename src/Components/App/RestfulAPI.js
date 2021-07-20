35d531af513c11af95a6c94990d4ac6aeda3614f
Karen Garcia <saykaren@gmail.com>
2021-07-20 15:53:05 -0600

    Adds Restful API resources

---
+++ src/Components/App/RestfulAPI.js
@@ -0,0 +1,88 @@
+import React from "react";
+
+const RestfulAPI = () => {
+  return (
+    <div className="instructionalBox">
+      <ul>
+        <li>
+          <a
+            href="https://www.restapitutorial.com/lessons/whatisrest.html"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            {" "}
+            What is REST Video
+          </a>
+        </li>
+        <li>
+          <a
+            href="https://www.freecodecamp.org/news/build-restful-api-with-authentication-under-5-minutes-using-loopback-by-expressjs-no-programming-31231b8472ca/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            How to Build a Restful API with Authentication in 5 Minutes
+          </a>
+        </li>
+
+        <li>
+          <a
+            href="https://www.youtube.com/watch?v=BdqPDxrfDOI"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Node and React JS Tutorial - Create An API
+          </a>
+        </li>
+        <li>
+          <a
+            href=" https://www.youtube.com/watch?v=BdqPDxrfDOI&t=67s"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Node and React JS Tutorial - Create An API
+          </a>
+        </li>
+
+        <li>
+          <a
+            href="http://jsonplaceholder.typicode.com/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Free Fack API for testing
+          </a>
+        </li>
+        <li>
+          <a
+            href="https://docs.mongodb.com/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            MongoDB Documentation
+          </a>
+        </li>
+        <li>
+          <a
+            href="https://www.youtube.com/watch?v=hzLDsxPGctY"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            React js Tutorial - How to Get Data from an API with React
+          </a>
+        </li>
+        <li>
+          <a
+            href="https://jsonplaceholder.typicode.com/users"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            {" "}
+            API Fake Data users
+          </a>
+        </li>
+      </ul>
+    </div>
+  );
+};
+
+export default RestfulAPI;
