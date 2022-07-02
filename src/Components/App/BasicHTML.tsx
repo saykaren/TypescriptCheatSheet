d4ae2ca98489d9a94f393f156221886413b6fbc1
Karen Garcia <saykaren@gmail.com>
2022-07-02 09:49:00 -0600

    369 html standards

--- src/Components/App/BasicHTML.tsx
+++ src/Components/App/BasicHTML.tsx
@@ -2,13 +2,23 @@ import React from "react";
 
 const BasicHTML = () => {
   return (
-    <>
-      <p>
+    <ul>
+      <li>
         {
           '<a href="https://saykaren.com" target="_blank" rel="noopener noreferrer"> sayKaren</a>'
         }
-      </p>
-    </>
+      </li>
+      <li>
+        <a
+          href="https://html.spec.whatwg.org/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          {" "}
+          HTML Standards
+        </a>
+      </li>
+    </ul>
   );
 };
 
