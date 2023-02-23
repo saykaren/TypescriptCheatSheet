6a74b68c5b27c19bf03e8e5e1a610a8c48a0c79b
Karen Garcia <saykaren@gmail.com>
2023-02-22 20:22:23 -0700

    437

---
+++ src/Components/Data/LanguagesData.js
@@ -0,0 +1,28 @@
+export const LanguagesData = [
+  {
+    technique: "String to lowercase",
+    php: "strtolower()",
+    javascript: "toLowerCase() or toLocaleLowerCase()",
+    note: "",
+  },
+  {
+    technique: "Array contains specific value",
+    php: "in_array('Apple', $array)",
+    javascript: "array.includes('Apple')",
+    note: "See Checks if a given key or index exists for checking key/index rather than value",
+  },
+  {
+    technique: "Check type is array",
+    php: "is_array($array)",
+    javascript: "Array.isArray(value)",
+    note: "",
+  },
+  {
+    technique: "Checks if a given key or index exists",
+    php: "array_key_exists('id', $array)",
+    javascript: "obj.hasOwnProperty('key')",
+    note: "",
+  },
+];
+
+export default LanguagesData;
